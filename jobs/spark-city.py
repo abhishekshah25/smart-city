87b35a994054684e44a750c8a44638e1231d1bdf
Abhishek Shah <147745895+abhishekshah25@users.noreply.github.com>
2024-05-23 21:43:00 +0530

    Update spark-city.py

--- jobs/spark-city.py
+++ jobs/spark-city.py
@@ -5,7 +5,7 @@ from pyspark.sql.functions import from_json, col
 from pyspark.sql.types import StructType,StructField, StringType,TimestampType, IntegerType, DoubleType
 
 def main():
-  spark = SparkSession.builder.appName("SmartCityStreaming")\
+  spark = SparkSession.builder.appName("Smart City Streaming")\
   .config("spark.jars.packages", 
           "org.apache.spark:spark-sql-kafka-0-10_2.13:3.5.0",
           "org.apache.hadoop:hadoop-aws:3.3.1",
